5c7d8ec15d2f95a1f75c386999aa23a302525b0d
Theodore Ts'o <tytso@mit.edu>
2015-01-22 15:20:55 -0500

    non-ext4 portions of "direct-io: Implement generic deferred AIO completions"
    Originally from 7b7a8665edd8db73
    
    Change-Id: I1c9baf97f18542c345f3139cf19ffff6dd5a4af3
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Theodore Ts'o <tytso@google.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -47,6 +47,7 @@ struct swap_info_struct;
 struct seq_file;
 struct fscrypt_info;
 struct fscrypt_operations;
+struct workqueue_struct;
 
 extern void __init inode_init(void);
 extern void __init inode_init_early(void);
@@ -64,8 +65,7 @@ struct buffer_head;
 typedef int (get_block_t)(struct inode *inode, sector_t iblock,
 			struct buffer_head *bh_result, int create);
 typedef void (dio_iodone_t)(struct kiocb *iocb, loff_t offset,
-			ssize_t bytes, void *private, int ret,
-			bool is_async);
+			ssize_t bytes, void *private);
 
 #define MAY_EXEC		0x00000001
 #define MAY_WRITE		0x00000002
@@ -1337,6 +1337,9 @@ struct super_block {
 
 	/* Being remounted read-only */
 	int s_readonly_remount;
+
+	/* AIO completions deferred from interrupt context */
+	struct workqueue_struct *s_dio_done_wq;
 };
 
 /* superblock cache pruning functions */
