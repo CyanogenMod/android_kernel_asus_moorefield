a03343a773688c494ee54b0c230c32c0ad07432b
Sean Paul <seanpaul@chromium.org>
2015-03-06 15:33:38 -0500

    drm: Remove temp DSI+Virtual connector types
    We're tracking upstream now, and userspace has switched
    over. Remove the temp values, they've run their course.
    
    Bug: 19118845
    Signed-off-by: Sean Paul <seanpaul@chromium.org>

--- include/uapi/drm/drm_mode.h
+++ include/uapi/drm/drm_mode.h
@@ -216,8 +216,6 @@ struct drm_mode_get_encoder {
 #define DRM_MODE_CONNECTOR_eDP		14
 #define DRM_MODE_CONNECTOR_VIRTUAL      15
 #define DRM_MODE_CONNECTOR_DSI		16
-#define DRM_MODE_CONNECTOR_TMP_DSI	0xe
-#define DRM_MODE_CONNECTOR_TMP_VIRTUAL  0xf
 
 struct drm_mode_get_connector {
 
