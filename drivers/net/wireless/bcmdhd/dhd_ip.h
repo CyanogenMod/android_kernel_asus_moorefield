b5ef0e3fb53c849e878baeb326501f591b880e35
Christopher R. Palmer <crpalmer@gmail.com>
2015-08-20 06:18:14 -0400

    bcmdhd: Increase number of src/dst ip/ports the driver can track
    This silences an error I'm seeing in the dmesg when doing lots
    of network requests (such as installing a bunch of apps after
    a clean install).
    
    Change-Id: I121c1ffc1d0f78924d583d2e9d05a13893891569

--- drivers/net/wireless/bcmdhd/dhd_ip.h
+++ drivers/net/wireless/bcmdhd/dhd_ip.h
@@ -36,8 +36,8 @@ extern pkt_frag_t pkt_frag_info(osl_t *osh, void *p);
 #define	TCPACKSZMAX	(TCPACKSZMIN + 100)
 
 /* Max number of TCP streams that have own src/dst IP addrs and TCP ports */
-#define TCPACK_INFO_MAXNUM 4
-#define TCPDATA_INFO_MAXNUM 4
+#define TCPACK_INFO_MAXNUM 32
+#define TCPDATA_INFO_MAXNUM 32
 #define TCPDATA_PSH_INFO_MAXNUM (8 * TCPDATA_INFO_MAXNUM)
 
 #define TCPDATA_INFO_TIMEOUT 5000	/* Remove tcpdata_info if inactive for this time (in ms) */
