d6e8380b380036c8b834a17fbfe052d31f9469ce
Patrick Tjin <pattjin@google.com>
2016-01-21 15:15:45 -0800

    perf: check reserved attribute is 0 on open
    Bug: 25364034
    
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -6708,6 +6708,9 @@ SYSCALL_DEFINE5(perf_event_open,
 	if (err)
 		return err;
 
+	if (attr.__reserved_1)
+		return -EINVAL;
+
 	if (!attr.exclude_kernel) {
 		if (perf_paranoid_kernel() && !capable(CAP_SYS_ADMIN))
 			return -EACCES;
