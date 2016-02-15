9a270efc0e14f8738c6c1621b0c52ee58e377e14
Christopher R. Palmer <crpalmer@gmail.com>
2016-02-15 07:34:46 -0500

    moorefield: Remove the set_cpufreq_boost in the wifi driver
    This crashes if you aren't using the interactive governor and
    rather than fix this hacky code, let's trust our governors to
    handle load correctly and just remove all of it.
    
    Change-Id: I9c3c9957754e5d30884417bed7325273cef9d34d

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -440,8 +440,6 @@ void cpufreq_frequency_table_update_policy_cpu(struct cpufreq_policy *policy);
 
 void cpufreq_frequency_table_put_attr(unsigned int cpu);
 
-void set_cpufreq_boost(unsigned long val);
-
 /*********************************************************************
  *                         CPUFREQ STATS                             *
  *********************************************************************/
